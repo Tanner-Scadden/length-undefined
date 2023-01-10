2f75eb9d946e711473209a89436c64fb899179f2
TannerScadden <tanner@scaddenfamily.com>
2023-01-10 18:59:07 -0500

    Created reproduction

---
+++ src/routes/index.tsx
@@ -0,0 +1,44 @@
+import { createMemo } from "solid-js";
+import { A, useRouteData } from "solid-start";
+import { createServerData$ } from "solid-start/server";
+import Counter from "~/components/Counter";
+
+export function routeData() {
+  return createServerData$(async () => {
+    await import("@neo4j/graphql-ogm");
+    return "test";
+  });
+}
+export default function Home() {
+  const data = useRouteData<typeof routeData>();
+
+  // force it to call the server data
+  createMemo(() => console.log(data()));
+
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        Hello world!
+      </h1>
+      <Counter />
+      <p class="mt-8">
+        Visit{" "}
+        <a
+          href="https://solidjs.com"
+          target="_blank"
+          class="text-sky-600 hover:underline"
+        >
+          solidjs.com
+        </a>{" "}
+        to learn how to build Solid apps.
+      </p>
+      <p class="my-4">
+        <span>Home</span>
+        {" - "}
+        <A href="/about" class="text-sky-600 hover:underline">
+          About Page
+        </A>{" "}
+      </p>
+    </main>
+  );
+}
